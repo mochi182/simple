951cb6daa008f22141681add30a10ae48b62e4ce
mochi182 <mochi182tv@gmail.com>
2022-09-19 23:07:57 -0500

    Puertos

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+
+const createError = require('http-errors');
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 80 || 8080 || 3000
+
+// ...
 
 app.get('/', (req, res) => {
   res.send('<h1>API simple</h1>')
@@ -8,7 +12,21 @@ app.get('/', (req, res) => {
 
 app.get('/ruta', (req, res) => {
     res.send('<h1>Ruta</h1>')
-  })
+})
+
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+
+app.use(function(err, req, res) {
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
